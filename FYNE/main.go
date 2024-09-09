78ce1106fddf9b4bda975dd296263a9193bde712
Franklin Diaz <franklin83diaz@gmail.com>
2024-09-09 07:33:26 -0400

    argument

--- FYNE/main.go
+++ FYNE/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	w "fynedemo/widgets/entryExample"
 
@@ -10,18 +11,39 @@ import (
 )
 
 func main() {
-	myApp := app.New()
-	myWindow := myApp.NewWindow("Fyne Demo")
 
-	// Handle key events at the window level
-	myWindow.Canvas().SetOnTypedKey(func(keyEvent *fyne.KeyEvent) {
-		if keyEvent.Name == fyne.KeyF1 {
-			fmt.Println("General help")
-		}
-	})
+	if len(os.Args) < 2 {
+		invalid()
+		return
+	}
 
-	myWindow.SetContent(w.EntryAndCustomEntry())
-	myWindow.Resize(fyne.NewSize(300, 200))
-	myWindow.ShowAndRun()
+	//TODO: refactor this code to use a switch statement
+	if os.Args[1] == "entry" {
 
+		myApp := app.New()
+		myWindow := myApp.NewWindow("Fyne Demo")
+
+		// Handle key events at the window level
+		myWindow.Canvas().SetOnTypedKey(func(keyEvent *fyne.KeyEvent) {
+			if keyEvent.Name == fyne.KeyF1 {
+				fmt.Println("General help")
+			}
+		})
+		myWindow.SetContent(w.EntryAndCustomEntry())
+		myWindow.Resize(fyne.NewSize(300, 200))
+		myWindow.ShowAndRun()
+	} else {
+		invalid()
+	}
+
+}
+
+func invalid() {
+	fmt.Println("Invalid argument")
+	fmt.Println("List of valid arguments: ")
+	//set color green
+	fmt.Print("\033[32m")
+	fmt.Println("entry")
+	//reset color
+	fmt.Print("\033[0m\n")
 }
